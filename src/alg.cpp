de781f49b2e1e4de7c25fd0cd40900e83f327ee1
VarvaraBelova23 <160231633+VarvaraBelova23@users.noreply.github.com>
2024-02-28 20:41:50 +0300

    b51 Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,46 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value <= 1) {
+        return false;
+    }
+    for (uint64_t i = 2; i * i <= value; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    if (n == 1) return 2;
+    uint64_t count = 1;
+    uint64_t number = 3;
+    while (count < n) {
+        if (checkPrime(number)) {
+            count++;
+        }
+        if (count < n) {
+            number += 2;
+        }
+    }
+    return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    uint64_t next = value + 1;
+    while (!checkPrime(next)) {
+        next++;
+    }
+    return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t summ = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            summ += i;
+        }
+    }
+    return summ;
 }
